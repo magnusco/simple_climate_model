00154214bdbb06fd2780169a436accdb4678ed76
magnusco <magnus.orke@lyse.net>
2020-10-29 17:10:25 +0100

    new params for system

--- simple_climate_model.py
+++ simple_climate_model.py
@@ -17,7 +17,7 @@ class TableParameters:
     a_LC: float = 0.622
     a_LW: float = 0.8358
     e_E: float = 1.0
-    e_A: float = 0.875
+    e_A: float = 2 * 0.875
     f_A: float = 0.618
     alpha: float = 3
     beta: float = 4
@@ -27,33 +27,47 @@ class TableParameters:
 if __name__ == "__main__":
 
     p = TableParameters()
+
+    gamma_0 = (1 - p.r_SM) * (1 - p.C_C * p.r_SC) * (1 - p.a_SW) * (1 - p.a_O3) * (1 - p.C_C * p.a_SC)
+    lambda_0 = p.C_C * p.r_LC * (1 - p.r_LE)
+    mu_0 = p.f_A * (1 - p.C_C * p.r_LC) * (1 - p.C_C * p.a_LC) * (1 - p.r_LE)
+
+    gamma_1 = ((1 - p.r_SM) * p.a_SW + (1 - p.r_SM) * (1 - p.a_SW) * p.a_O3 + (1 - p.r_SM) * (1 - p.a_SW) * (1 - p.a_O3) * (1 - p.C_C * p.r_SC) * p.C_C * p.a_SC)
+    lambda_1 = (1 - p.C_C * p.r_LC) * p.C_C * p.a_LC + (1 - p.C_C * p.r_LC) * (1 - p.C_C * p.a_LC) * p.a_LW
+    mu_1 = p.f_A * p.C_C * p.r_LC * p.a_LW
     
-    a_0 = p.e_E * p.sigma * (1 - p.C_C * p.r_LC * (1 - p.r_LE))
-    b_0 = p.f_A * p.e_A * p.sigma * (p.r_LE - 1)
+    a_0 = p.sigma * p.e_E * (1 - lambda_0)
+    b_0 = - p.sigma * p.e_E * mu_0
     c_0 = p.alpha
     d_0 = - p.beta
-    e_0 = (1 - p.r_SM) * (1 - p.C_C * p.r_SC) * (1 - p.a_SW) * (1 - p.a_O3) * (1 - p.C_C * p.a_SC) * p.P_s
+    e_0 = gamma_0 * p.P_s
 
-    a_1 = - p.e_E * p.sigma * ((1 - p.C_C * p.r_LC) * p.C_C * p.a_LC + (1 - p.C_C * p.r_LC) * (1 - p.C_C * p.a_LC) * p.a_LW)
-    b_1 = p.e_A * p.sigma
+    a_1 = - p.sigma * p.e_E * lambda_1
+    b_1 = p.sigma * p.e_A * (1 - mu_1)
     c_1 = - p.alpha
     d_1 = p.beta
-    e_1 = ((1 - p.r_SM) * p.a_SW + (1 - p.r_SM) * (1 - p.a_SW) * p.a_O3 + (1 - p.r_SM) * (1 - p.a_SW) * (1 - p.a_O3) * (1 - p.C_C * p.r_SC) * p.C_C * p.a_SC) * p.P_s
+    e_1 = gamma_1 * p.P_s
 
     A, E = np.array([[a_0, b_0, c_0, d_0], [a_1, b_1, c_1, d_1]]), np.array([e_0, e_1])
-
-    print(A)
-    print()
-    print(E)
 
 
     def F(temperature: np.ndarray):
         X = np.concatenate((np.power(temperature, 4), temperature))
         return np.matmul(A, X) - E
 
-    equilib_temp = scipy.optimize.anderson(F, np.array([280, 280]), f_tol = 1e-9)
+    equilib_temp = scipy.optimize.fsolve(F, np.array([280, 280]), xtol = 1e-9)
     print("Solution:  ", equilib_temp - 273.15)
-    
-    print(p.e_A * p.sigma * equilib_temp[1]**4)
 
-    
+    P_E_in = gamma_0 * p.P_s
+    P_A_in = gamma_1 * p.P_s
+    R_E_in = lambda_0 * p.sigma * p.e_E * equilib_temp[0]**4 + mu_0 * p.sigma * p.e_A * equilib_temp[1]**4
+    R_A_in = lambda_1 * p.sigma * p.e_E * equilib_temp[0]**4 + mu_1 * p.sigma * p.e_A * equilib_temp[1]**4
+    R_E_out = p.sigma * p.e_E * equilib_temp[0]**4
+    R_A_out = p.sigma * p.e_A * equilib_temp[1]**4
+    P_lat = (p.alpha + p.beta) * (equilib_temp[0] - equilib_temp[1])
+
+    print("Short-wave in: ", P_E_in, P_A_in)
+    print("Long-wave in: ", R_E_in, R_A_in)
+    print("Long-wave out: ", R_E_out, R_A_out)
+    print("Total latent: ", P_lat)
+
